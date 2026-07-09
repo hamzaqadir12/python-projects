c0e9aac811750fa2bffdedf1b89cfc175355057e
hamzaqadir12 <your-github-email@example.com>
2026-07-10 02:59:42 +0500

    cs50p: exception handling with try/except

--- practice.py
+++ practice.py
@@ -1,20 +1,14 @@
 def main():
-    number = get_number()
-    printer(number)
+    x = get_number()
+    print(f"X is {x}")
 
 def get_number():
     while True:
-        n = int(input("Number: "))
-        if n > 0:
-            return n
-
-def printer(number):
-    # top half
-    for row in range(1, number + 1):
-        print(" " * (number - row) + "#" * (2 * row - 1))
-    
-    # bottom half
-    for row in range(1, number):
-        print(" " * row + "#" * (2 * (number - row) - 1))
+        try:
+            x = int(input("X: "))
+            if 0 <= x <=100:
+                return x
+        except ValueError:
+            print("Enter an integer")
 
 main()
