3333f0a6001551c75e103b16fe25c48b270f1ab9
hamzaqadir12 <your-github-email@example.com>
2026-07-11 04:07:13 +0500

    division using exceptions

--- practice.py
+++ practice.py
@@ -1,9 +1,14 @@
 def main():
-    x = get_number("X: ")
-    y = get_number("Y: ")
-    print(f"X is {x} and Y is {y}")
+    x = get_int("X: ")
+    while True:
+        y = get_int("Y: ")
+        try:
+            print(x / y)
+            break
+        except ZeroDivisionError:
+            pass
 
-def get_number(prompt):
+def get_int(prompt):
     while True:
         try:
             return int(input(prompt))
