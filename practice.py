d6d6c4efe2b9d8dca1588caba816c209fc21764a
hamzaqadir12 <hamzaqadir2019@gmail.com>
2026-07-13 02:28:03 +0500

    word counter using loops

--- practice.py
+++ practice.py
@@ -1,36 +1,13 @@
 def main():
-    x = get_num("X: ")
-    opp =get_opp()
+    sent = get_sentence()
+    print(len(sent.split()))
+
+def get_sentence():
     while True:
-        y = get_num("Y: ")
-        try:
-            print(round(get_calc(x, y, opp), 2))
-            break
-        except ZeroDivisionError:
-            pass
-
-def get_num(prompt):
-    while True:
-        try:
-            return float(input(prompt))
-        except ValueError:
-            pass
-
-def get_opp():
-    while True:
-        operator = input("Operator: ")
-        if operator in ["+", "-", "*", "/"]:
-            return operator
-
-def get_calc(x, y, opp):
-    if opp == "+":
-        return x + y
-    elif opp == "-":
-        return x - y
-    elif opp == "*":
-        return x * y
-    else:
-        return x / y
-
+        sent = input("Sentence: ")
+        if len(sent.split()) == 0:
+            print("Invalid input")
+        else:
+            return sent
 
 main()
