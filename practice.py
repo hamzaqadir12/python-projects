3b1a63a6f1be458181389e5e4b64525bd7242c1b
hamzaqadir12 <hamzaqadir2019@gmail.com>
2026-07-14 01:24:04 +0500

    calculator using exceptions and handling various errors

--- practice.py
+++ practice.py
@@ -1,14 +1,37 @@
 def main():
-    w = get_word()
-    if w == w[::-1]:
-        print("Palindrome")
-    else:
-        print("Not a palindrome")
-
-def get_word():
+    x = get_int("X: ")
+    y = get_int("Y: ")
+    opp = get_opp("Operator: ")
     while True:
-        word = input("Word: ").lower()
-        if word.strip():
-            return word
-    
+        try:
+            answer = get_calc(x, y, opp)
+            print(round(float(answer),2))
+            break
+        except ZeroDivisionError:
+            y = get_int("Y: ")
+
+def get_int(prompt):
+    while True:
+        try:
+            return int(input(prompt))
+
+        except ValueError:
+            pass
+
+def get_opp(prompt):
+    while True:
+        operator = input(prompt)
+        if operator in ["+", "-", "*", "/"]:
+            return operator
+
+def get_calc(x, y, opp):
+    if opp == "+":
+        return x + y
+    elif opp == "-":
+        return x - y
+    elif opp == "*":
+        return x * y
+    else:
+        return x / y
+
 main()
